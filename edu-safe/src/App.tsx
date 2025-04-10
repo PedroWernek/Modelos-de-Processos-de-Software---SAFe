6dc879da95aaaf81c26d18191c93dff924d77b10
PedroWernek <pedrowcnetto@gmail.com>
2025-04-10 13:10:00 -0300

    creating the browser router structure

--- edu-safe/src/App.tsx
+++ edu-safe/src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="App">
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
--- edu-safe/src/index.tsx
+++ edu-safe/src/index.tsx
@@ -1,12 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./routes/ErrorPage";
+import Home from "./routes/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      // dentro dessas chaves vocês irão colocar as paginas que criarem
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
