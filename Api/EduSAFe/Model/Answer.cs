95d9ef3d4ac25feeb26a318b6f0876ebb3fe5e2d
Ana Paula Endler <anapaulaendler@gmail.com>
2025-04-22 06:52:33 -0300

    IsCorrectAnswer na entidade Answer

--- Api/EduSAFe/Model/Answer.cs
+++ Api/EduSAFe/Model/Answer.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace EduSAFe.Model;
 
-// sinceramente, não entendi como vai funcionar... (não que esteja errado, mas queria conversar depois sobre)
 public class Answer
 {
     [Key]
     public Guid Id { get; set; }
     [Required]
     public string Text { get; set; } = null!;
+    public bool IsCorrectAnswer { get; set; }
 }
