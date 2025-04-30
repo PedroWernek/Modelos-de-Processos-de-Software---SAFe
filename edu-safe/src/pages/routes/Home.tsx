91e73d534e69f6eadf1bd7d643faaec5f8499486
Ana Paula Endler <anapaulaendler@gmail.com>
2025-04-29 21:10:14 -0300

    retirando exemplo da Home

--- edu-safe/src/pages/routes/Home.tsx
+++ edu-safe/src/pages/routes/Home.tsx
@@ -22,12 +22,6 @@ const Home = () => {
             A EDUSAFe possui o intuito de permitir que você aprenda de forma
             divertida como aplicar a SAFe em seu ambiente de trabalho!
           </p>
-          <InfoCard
-            title="PINA COLADA SONG"
-            description="if you like pinaaaa coladas and getting blabla n lembro o resot da letrsa taltal if you like making love at midnight blabla im the one that youre looking for Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-            borderColor="pink"
-            icon={iconDELETAR}
-          />
           <CustomButton
             backgroundColor="#1c1f2c"
             hasRambow={true}
