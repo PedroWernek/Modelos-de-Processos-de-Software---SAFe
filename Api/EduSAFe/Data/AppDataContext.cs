ec29a0569f9d51016d41186532b619f6bf95369d
ana paula <ana.endler@headsoft.com.br>
2025-05-09 13:48:14 -0300

    teste

--- Api/EduSAFe/Data/AppDataContext.cs
+++ Api/EduSAFe/Data/AppDataContext.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using EduSAFe.Enums;
-using EduSAFe.Model;
+using EduSAFe.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace EduSAFe.Data;
